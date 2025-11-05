9ff3c2e50e03637cdbb9f3606f4b85cbe131f967
bkattenburg <bkattenburg@gmail.com>
2025-11-05 15:20:08 -0800

    Update app-98bi-vague.py

--- app-98bi-vague.py
+++ app-98bi-vague.py
@@ -2132,7 +2132,7 @@ with tab_objects[1]:
         if st.session_state.get("client_tax_id"):
             st.session_state["client_id"] = st.session_state["client_tax_id"]
             prof_client_id = st.session_state["client_id"] # Also update the local variable for the widget
-        if st.session_state.get('selectedenv') == "Onit ELM VAT":
+        if st.session_state.get('selected_env') == "Onit ELM VAT":
             st.info(
         "Note: For Onit ELM VAT profiles, please review the LEDES 1998BI section in Help & FAQs. Ensure your matter setup and invoice entries meet the specific requirements for VAT.\n"
         "See sidebar Help & FAQs for mandatory fields and setup guidance."
@@ -2673,6 +2673,7 @@ if "generated_files" in st.session_state and st.session_state.generated_files:
                 key=f"download_{filename}" # Unique key is important
             )
         col_idx += 1
+
 
 
 
