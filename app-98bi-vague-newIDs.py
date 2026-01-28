d3692ce98696386503622496c24c5ced04fe1ebf
bkattenburg <bkattenburg@gmail.com>
2026-01-28 13:05:24 -0800

    Update download button filenames for OnitX data

--- app-98bi-vague-newIDs.py
+++ app-98bi-vague-newIDs.py
@@ -2027,12 +2027,12 @@ with st.sidebar.expander("Timekeeper Downloads"):
     # OnitX_SS&E Timekeepers
     onitx_SSE_eur_tk_data = read_file_for_download("assets/onitx_SS&E_eur_tk.csv")
     if onitx_SSE_eur_tk_data:
-        st.download_button("OnitX - SS&E - EUR", onitx_SSE_eur_tk_data, "onitx_SS&E_eur_tk.csv", "text/csv")
+        st.download_button("OnitX - SS&E - EUR", onitx_SSE_eur_tk_data, "OnitX_SS&E_EUR_tk.csv", "text/csv")
     
     # OnitX Timekeepers - VAT
     onitx_Nelson_eur_tk_data = read_file_for_download("assets/onitx_Nelson_eur_tk.csv")
     if onitx_Nelson_eur_tk_data:
-        st.download_button("OnitX - Nelson - EUR", onitx_Nelson_eur_tk_data, "onitx_eur_Nelson_tk.csv", "text/csv")
+        st.download_button("OnitX - Nelson - EUR", onitx_Nelson_eur_tk_data, "OnitX_Nelson_EUR_tk.csv", "text/csv")
 
     # SimpleLegal Timekeepers
     sl_tk_data_jdc = read_file_for_download("assets/simplelegal_tk - JDC.csv")
