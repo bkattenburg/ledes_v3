ea3112294d0e6e1c1872dd9f7845f0d937c830b6
bkattenburg <bkattenburg@gmail.com>
2026-01-28 13:01:51 -0800

    Update file paths for OnitX SS&E and Nelson timekeepers

--- app-98bi-vague-newIDs.py
+++ app-98bi-vague-newIDs.py
@@ -2025,14 +2025,14 @@ with st.sidebar.expander("Timekeeper Downloads"):
         st.download_button("OnitX", onitx_tk_data, "onitx_tk.csv", "text/csv")
 
     # OnitX_SS&E Timekeepers
-    onitx_SSE_tk_data = read_file_for_download("assets/onitx_SS&E_tk.csv")
-    if onitx_SSE_tk_data:
-        st.download_button("OnitX - SS&E - EUR", onitx_SSE_tk_data, "onitx_SS&E_eur_tk.csv", "text/csv")
+    onitx_SS&E_eur_tk_data = read_file_for_download("assets/onitx_SS&E_eur_tk.csv")
+    if onitx_SS&E_eur_tk_data:
+        st.download_button("OnitX - SS&E - EUR", onitx_SS&E_eur_tk_data, "onitx_SS&E_eur_tk.csv", "text/csv")
     
     # OnitX Timekeepers - VAT
-    onitx_eur_nelson_tk_data = read_file_for_download("assets/onitx_nelson_eur_tk.csv")
-    if onitx_eur_nelson_tk_data:
-        st.download_button("OnitX - Nelson - EUR", onitx_eur_nelson_tk_data, "onitx_eur_Nelson_tk.csv", "text/csv")
+    onitx_Nelson_eur_tk_data = read_file_for_download("assets/onitx_Nelson_eur_tk.csv")
+    if onitx_Nelson_eur_tk_data:
+        st.download_button("OnitX - Nelson - EUR", onitx_Nelson_eur_tk_data, "onitx_eur_Nelson_tk.csv", "text/csv")
 
     # SimpleLegal Timekeepers
     sl_tk_data_jdc = read_file_for_download("assets/simplelegal_tk - JDC.csv")
